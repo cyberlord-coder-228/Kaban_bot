6ae08cb3e12aa70d9842a2378c061f7151371111
cyberlord-coder-228 <72409641+cyberlord-coder-228@users.noreply.github.com>
2021-03-03 21:46:42 +0200

    Create MyArrs_kaban.js

---
+++ MyArrs_kaban.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const SubjectsArr = [...]; //here is subjects for random generated jokes
+
+const ActionsArr = [...]; //here is verbs for random generated jokes
+
+const FunnyPicturesArr = [...]; //here is links to memes/ funny pictures
+
+const PunsArr = [...]; //here is... well, puns
+
+const QuotesArr = [...]; //you want believe me. quotes goes here
+
+const ResponsesArr = [...]; //responses for /answer command
+
+const RepliesArr = [...]; //replies for /pidor command
+
+module.exports = {
+  SubjectsArr,
+  ActionsArr,
+  FunnyPicturesArr,
+  PunsArr,
+  QuotesArr,
+  ResponsesArr,
+  RepliesArr
+};
