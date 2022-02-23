7ea6bfb1538f55f163cc4a2f716ac2953d4e6c55
Vladyslav Kravchenko <72409641+cyberlord-coder-228@users.noreply.github.com>
2022-02-23 17:26:34 +0200

    sudoers.mjs updated

--- sudoers.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const sudoers = {
-  'name_example':420420420,
-};
-
-module.exports = { sudoers };
---
+++ sudoers.mjs
@@ -0,0 +1,5 @@
+'use strict';
+
+export const sudoers = {
+  'name_example': 420420420,
+};
