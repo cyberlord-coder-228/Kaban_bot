b7f007caf8bb88120176ff6cc53d38b0f3411c8a
Vladyslav Kravchenko <72409641+cyberlord-coder-228@users.noreply.github.com>
2022-02-23 17:24:54 +0200

    myArrs_kaban.mjs updated

---
+++ myArrs_kaban.mjs
@@ -0,0 +1,22 @@
+'use strict';
+
+const subjectsArr = [...]; //here is subjects for random generated jokes
+
+const actionsArr = [...]; //here is verbs for random generated jokes
+
+const funnyPicturesArr = [...]; //here is links to memes/funny pictures
+
+const punsArr = [...]; //here is... well, puns
+
+const quotesArr = [...]; //you wont believe me. quotes goes here
+
+const answersArr = [...]; //responses for /answer command
+
+export default {
+  subjectsArr,
+  actionsArr,
+  funnyPicturesArr,
+  punsArr,
+  quotesArr,
+  answersArr,
+};
